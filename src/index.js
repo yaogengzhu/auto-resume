06f29909a9e5610b28a7d0e7383cbfdf2a180572
edz <edz@edzdeMacBook-Pro-3.local>
2019-01-11 09:47:50 +0800

    添加了babelrc文件

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import resumeMd from './md/resume.md'
 import './css/main.css'
 
 document.getElementById('markdownContainer').innerHTML = resumeMd
-var victor = new Victor("bgContainer", "output");
-var theme = [
+const victor = new Victor("bgContainer", "output");
+const theme = [
     ["#002c4a", "#005584"],
     ["#35ac03", "#3f4303"]
 ]
+
 victor(theme[Math.ceil(Math.random() * 2) - 1]).set();
